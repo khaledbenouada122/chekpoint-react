80f7453a24493a5d71b33aa82dba4c968f1b3797
khaledbenouada122 <khaled.benouada@polytechnien.tn>
2022-08-11 15:20:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nabvar from './compoments/Nabvar'
+import { Forme } from './compoments/Forme';
+
+function App() {
+  return (
+    <div className="App">
+      <Nabvar></Nabvar>
+      <Forme></Forme>
+    </div>
+  );
+}
+
+export default App;
